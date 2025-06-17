e3b9191161ec0442e506d89fae561b6441420e76
Dr-Jones-xo <jobr0005@stud.kea.dk>
2025-06-17 16:29:35 +0200

    Update esp_listenter.py

--- esp_listenter.py
+++ esp_listenter.py
@@ -2,24 +2,20 @@ import sqlite3
 import paho.mqtt.client as mqtt
 from datetime import datetime
 
-# MQTT login og broker
 BROKER = "localhost"
 PORT = 1883
 USERNAME = "hamsa"
 PASSWORD = "12345678"
 TOPIC = "sensor/dht11"
 
-# Callback når forbindelsen oprettes
 def on_connect(client, userdata, flags, rc):
     print("Forbundet til broker med kode:", rc)
     client.subscribe(TOPIC)
 
-# Callback når en besked modtages
 def on_message(client, userdata, msg):
     data = msg.payload.decode()
     print(f"[MQTT] {msg.topic}: {data}")
 
-    # Forventet data-format: "temp,hum" (fx "23.5,45")
     try:
         temp, hum = map(float, data.split(","))
         now = datetime.now().strftime("%d/%m/%y %H:%M:%S")
@@ -31,7 +27,6 @@ def on_message(client, userdata, msg):
     except Exception as e:
         print("Fejl ved indsættelse i database:", e)
 
-# Opret klient og forbind
 client = mqtt.Client()
 client.username_pw_set(USERNAME, PASSWORD)
 client.on_connect = on_connect
